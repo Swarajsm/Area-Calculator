8eb535356b44a55731db9714102ece3813241064
Swaraj Mahindre <swarajmahindre5@gmail.com>
2020-05-10 14:56:42 +0530

    Update Area_calculator.py

--- Area_calculator.py
+++ Area_calculator.py
@@ -1,11 +1,11 @@
 #first of all I will begin with taking valid inputs from user
 #This should include whether it's a Circle or a Triangle
-Object= (str(input("Enter C for circle and T for Triangle:    "))
-if(Object==C):
+Object= (input("Enter C for circle and T for Triangle:    ")
+if(Object=="C"):
   r= float(input("Enter the Radius"))
   area= 3.14 * r *r
   print("Area:",area)
-elif(Object==T):
+elif(Object=="T"):
   base=float(input("ENter the value of base"))
   height=float(input("Enter the value of Height"))
   Area= 1/2 * base * height
